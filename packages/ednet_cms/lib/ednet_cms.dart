cf0d5f0d89acd06c3386231ee7d9a07f8cce6281
slavisam <slavisam@gmail.com>
2023-01-23 14:29:12 +0100

    Add Content, codemagic badge and coverage data

--- packages/ednet_cms/lib/ednet_cms.dart
+++ packages/ednet_cms/lib/ednet_cms.dart
@@ -4,3 +4,28 @@ library ednet_cms;
 abstract class Content<T> {
   T get value;
 }
+
+abstract class TextContent implements Content<String> {}
+
+class Text implements TextContent {
+  @override
+  final String value;
+
+  const Text(this.value);
+}
+
+class Number implements Content<num> {
+  @override
+  final num value;
+
+  const Number(this.value);
+}
+
+
+/// again Date and
+class Date implements Content<DateTime> {
+  @override
+  final DateTime value;
+
+  const Date(this.value);
+}
