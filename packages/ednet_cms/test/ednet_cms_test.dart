8ed6aab6dce3d86eac97a1b8c1cab809b9139c1d
slavisam <slavisam@gmail.com>
2023-01-23 13:31:27 +0100

    Comment out default test

--- packages/ednet_cms/test/ednet_cms_test.dart
+++ packages/ednet_cms/test/ednet_cms_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ednet_cms/ednet_cms.dart';
+// import 'package:ednet_cms/ednet_cms.dart';
 
 void main() {
   test('adds one to input values', () {
