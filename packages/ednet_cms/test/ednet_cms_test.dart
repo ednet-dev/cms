955d7444ba41a76a6bc0c17f314ae6a76ba5fa7d
slavisam <slavisam@gmail.com>
2023-01-23 15:00:52 +0100

    Cleanup CMS pubspec.yaml

--- packages/ednet_cms/test/ednet_cms_test.dart
+++ packages/ednet_cms/test/ednet_cms_test.dart
@@ -1,5 +1,5 @@
 import 'package:ednet_cms/ednet_cms.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 
 void main() {
   group('That Content have vale of correct type, be that ', () {
