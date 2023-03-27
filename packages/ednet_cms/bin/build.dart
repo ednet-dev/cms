e67ff49ae5402c58c55768a84895a3a61e244dc5
slavisam <slavisam@gmail.com>
2023-03-27 17:36:16 +0200

    sync(ednet_cms): parse client path

--- packages/ednet_cms/bin/build.dart
+++ packages/ednet_cms/bin/build.dart
@@ -1,3 +1,12 @@
-main() {
-  print('I AM BUILDING!');
+import 'dart:io';
+import 'package:path/path.dart' as p;
+
+void main(List<String> arguments) {
+  // Get the path of the build.dart script.
+  Uri scriptUri = Platform.script;
+
+  // Resolve the path of the client project folder, assuming the script is called from the root folder.
+  String clientAppFolderPath = p.dirname(p.dirname(p.fromUri(scriptUri)));
+
+  print('The path of the client project folder is: $clientAppFolderPath');
 }
