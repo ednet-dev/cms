37fff85593d97d530ec12cd55097ee033cdec292
slavisam <slavisam@gmail.com>
2023-11-25 12:12:55 +0100

    Generate PDF.

--- tools/apdf/lib/src/commands/generate_pdf_command.dart
+++ tools/apdf/lib/src/commands/generate_pdf_command.dart
@@ -4,6 +4,7 @@ import 'package:args/command_runner.dart';
 import 'package:glob/glob.dart';
 import 'package:mason_logger/mason_logger.dart';
 import 'package:path/path.dart' as path;
+import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 
 class GeneratePdfCommand extends Command<int> {
@@ -28,7 +29,7 @@ class GeneratePdfCommand extends Command<int> {
         abbr: 'i',
         help: 'Glob pattern for files to include.',
         valueHelp: 'include',
-        defaultsTo: '*.dart',
+        defaultsTo: '**/*.dart',
       )
       ..addOption(
         'exclude',
@@ -57,19 +58,32 @@ class GeneratePdfCommand extends Command<int> {
     final sourcePath = argResults?['source'] as String? ?? '.';
     final outputFileName =
         argResults?['output'] as String? ?? 'source_code.pdf';
-    final includePattern = argResults?['include'] as String? ?? '*.dart';
-    final excludePattern = argResults?['exclude'] as String? ?? '';
+    final includePatterns = argResults?['include'] as String? ?? '**/*.dart';
+    final excludePatterns = argResults?['exclude'] as String? ?? '';
+
+    final includeGlobs = includePatterns.isNotEmpty
+        ? includePatterns
+            .split(',')
+            .map((pattern) => Glob(pattern, recursive: true))
+            .toList()
+        : [
+            Glob('**/*.dart', recursive: true)
+          ]; // Default to Dart files if empty
+
+    final excludeGlobs = excludePatterns.isNotEmpty
+        ? excludePatterns
+            .split(',')
+            .map((pattern) => Glob(pattern, recursive: true))
+            .toList()
+        : <Glob>[]; // Default to an empty list if exclude pattern is empty
     final sourceDirectory = Directory(sourcePath);
     final codeBuffer = StringBuffer();
-
-    final includeGlob = Glob(includePattern, recursive: true);
-    final excludeGlob = Glob(excludePattern, recursive: true);
 
     await traverseDirectory(
       sourceDirectory,
       codeBuffer,
-      includeGlob,
-      excludeGlob,
+      includeGlobs,
+      excludeGlobs,
     );
     final allCode = codeBuffer.toString();
 
@@ -82,43 +96,61 @@ class GeneratePdfCommand extends Command<int> {
   Future<void> traverseDirectory(
     Directory directory,
     StringBuffer codeBuffer,
-    Glob includeGlob,
-    Glob excludeGlob,
+    List<Glob> includeGlobs,
+    List<Glob> excludeGlobs,
   ) async {
     final rootPath = directory.absolute.path;
 
     await for (final entity
         in directory.list(recursive: true, followLinks: false)) {
       final filePath = entity.absolute.path;
-
+      // Compute the relative file path using rootPath to ensure consistency
       final relativeFilePath = path.relative(filePath, from: rootPath);
-      final isPathMatch = includeGlob.matches(relativeFilePath);
-      print(isPathMatch);
-      if (entity is File &&
-          includeGlob.matches(entity.path) &&
-          !excludeGlob.matches(entity.path)) {
-        final code = await entity.readAsString();
-        codeBuffer
-          ..write(code.trim())
-          ..write('\n\n\n'); // Add some space between files in the PDF. } } }
+
+      if (entity is File) {
+        final isIncluded =
+            includeGlobs.any((glob) => glob.matches(relativeFilePath));
+        final isExcluded =
+            excludeGlobs.any((glob) => glob.matches(relativeFilePath));
+
+        if (isIncluded && !isExcluded) {
+          _logger.info('Adding file: $relativeFilePath');
+          final code = await entity.readAsString();
+          codeBuffer
+            ..write(code.trim())
+            ..write('\n\n\n'); // Add some space between files in the PDF.
+        }
       }
     }
   }
 
   Future<void> generatePdf(String allCode, String outputFileName) async {
-    final pdf = pw.Document()
-      ..addPage(
-        pw.Page(
-          build: (pw.Context context) {
-            return pw.Center(
-              child: pw.Text(
-                allCode,
-                style: pw.TextStyle(font: pw.Font.courier(), fontSize: 10),
-              ),
-            );
-          },
-        ),
-      );
+    final pdf = pw.Document();
+
+    // Define the maximum length of text that can go on one page
+    const maxChunkSize = 1000; // Adjust this size as needed
+
+    // Split the allCode into smaller chunks
+    var codeChunks = <String>[];
+    for (var i = 0; i < allCode.length; i += maxChunkSize) {
+      var end = (i + maxChunkSize < allCode.length)
+          ? i + maxChunkSize
+          : allCode.length;
+      codeChunks.add(allCode.substring(i, end));
+    }
+
+    // Add each chunk as a separate paragraph to the PDF
+    pdf.addPage(
+      pw.MultiPage(
+        pageFormat: PdfPageFormat.a4,
+        build: (pw.Context context) => codeChunks.map((chunk) {
+          return pw.Paragraph(
+            text: chunk,
+            style: pw.TextStyle(font: pw.Font.courier(), fontSize: 10),
+          );
+        }).toList(),
+      ),
+    );
 
     final file = File(outputFileName);
     await file.writeAsBytes(await pdf.save());
