10b54ed30e45f562ea2d124397ab6d407bc0b486
slavisam <slavisam@gmail.com>
2024-05-26 15:39:50 +0200

    ref(core): Concept generation with template string.

--- packages/core/lib/gen/ednet_model_generic.dart
+++ packages/core/lib/gen/ednet_model_generic.dart
@@ -1,69 +1,91 @@
 part of ednet_core;
 
+/// Generates Dart code for model entries.
 String genEntries(Model model, String library) {
   Domain domain = model.domain;
 
-  var sc = 'part of $library; \n';
-  sc = '$sc \n';
-  sc = '$sc// lib/gen/${domain.codeLowerUnderscore}/'
-      '${model.codeLowerUnderscore}/model_entries.dart \n';
-  sc = '$sc \n';
-  sc = '${sc}class ${model.code}Entries extends ModelEntries { \n';
-  sc = '$sc \n';
-  sc = '$sc  ${model.code}Entries(Model model) : super(model); \n';
-  sc = '$sc \n';
-  sc = '$sc  Map<String, Entities> newEntries() { \n';
-  sc = '$sc    var entries = Map<String, Entities>(); \n';
-  sc = '$sc    var concept; \n';
+  // Start of the generated code.
+  String sc = '''
+part of $library;
+// Hello world from `dart run build_runner build` command :)
+// Generated code for model entries in lib/gen/${domain.codeLowerUnderscore}/${model.codeLowerUnderscore}/model_entries.dart
+
+class ${model.code}Entries extends ModelEntries {
+
+  ${model.code}Entries(Model model) : super(model);
+
+  /// Creates a map of new entries for each concept in the model.
+  Map<String, Entities> newEntries() {
+    var entries = Map<String, Entities>();
+    var concept;
+''';
+
+  // Loop through each entry concept to generate entries.
   for (Concept entryConcept in model.entryConcepts) {
-    sc = '$sc    concept = model.concepts.singleWhereCode('
-        '"${entryConcept.code}"); \n';
-    sc = '$sc    entries["${entryConcept.code}"] = '
-        '${entryConcept.codes}(concept); \n';
+    sc += '''
+    concept = model.concepts.singleWhereCode("${entryConcept.code}");
+    entries["${entryConcept.code}"] = ${entryConcept.codes}(concept);
+''';
   }
-  sc = '$sc    return entries; \n';
-  sc = '$sc  } \n';
-  sc = '$sc \n';
 
-  sc = '$sc  Entities? newEntities(String conceptCode) { \n';
-  sc = '$sc    var concept = model.concepts.singleWhereCode(conceptCode); \n';
-  sc = '$sc    if (concept == null) { \n';
-  sc = '$sc      throw ConceptError('
-      '"\${conceptCode} concept does not exist.") ; \n';
-  sc = '$sc    } \n';
+  sc += '''
+    return entries;
+  }
+
+  /// Returns a new set of entities for the given concept code.
+  Entities? newEntities(String conceptCode) {
+    var concept = model.concepts.singleWhereCode(conceptCode);
+    if (concept == null) {
+      throw ConceptError("\${conceptCode} concept does not exist.");
+    }
+''';
+
+  // Loop through each concept to match and return the correct entities.
   for (Concept concept in model.concepts) {
-    sc = '$sc    if (concept.code == "${concept.code}") { \n';
-    sc = '$sc      return ${concept.codes}(concept); \n';
-    sc = '$sc    } \n';
+    sc += '''
+    if (concept.code == "${concept.code}") {
+      return ${concept.codes}(concept);
+    }
+''';
   }
-  sc = '$sc    return null; \n';
-  sc = '$sc  } \n';
-  sc = '$sc \n';
 
-  sc = '$sc  Entity? newEntity(String conceptCode) { \n';
-  sc = '$sc    var concept = model.concepts.singleWhereCode(conceptCode); \n';
-  sc = '$sc    if (concept == null) { \n';
-  sc = '$sc      throw ConceptError('
-      '"\${conceptCode} concept does not exist.") ; \n';
-  sc = '$sc    } \n';
+  sc += '''
+    return null;
+  }
+
+  /// Returns a new entity for the given concept code.
+  Entity? newEntity(String conceptCode) {
+    var concept = model.concepts.singleWhereCode(conceptCode);
+    if (concept == null) {
+      throw ConceptError("\${conceptCode} concept does not exist.");
+    }
+''';
+
+  // Loop through each concept to match and return the correct entity.
   for (Concept concept in model.concepts) {
-    sc = '$sc    if (concept.code == "${concept.code}") { \n';
-    sc = '$sc      return ${concept.code}(concept); \n';
-    sc = '$sc    } \n';
+    sc += '''
+    if (concept.code == "${concept.code}") {
+      return ${concept.code}(concept);
+    }
+''';
   }
-  sc = '$sc    return null; \n';
-  sc = '$sc  } \n';
-  sc = '$sc \n';
 
+  sc += '''
+    return null;
+  }
+''';
+
+  // Getter for each entry concept.
   for (Concept entryConcept in model.entryConcepts) {
-    sc = '$sc  ${entryConcept.codes} get '
-        '${entryConcept.codesFirstLetterLower}'
-        ' => getEntry("${entryConcept.code}") as ${entryConcept.codes}; \n';
+    sc += '''
+  ${entryConcept.codes} get ${entryConcept.codesFirstLetterLower} => getEntry("${entryConcept.code}") as ${entryConcept.codes};
+''';
   }
 
-  sc = '$sc \n';
-  sc = '$sc} \n';
-  sc = '$sc \n';
+  // End of the generated code.
+  sc += '''
+}
+''';
 
   return sc;
 }
