39b42e847eeadb017209372d6d237386a707f63b
slavisam <slavisam@gmail.com>
2024-05-26 23:22:41 +0200

    feat(one): Entity widget.

--- packages/ednet_cms/lib/src/platforms/entity_widget.dart
+++ packages/ednet_cms/lib/src/platforms/entity_widget.dart
@@ -1,89 +1,175 @@
-// part of ednet_cms;
-//
-// class EntityWidget<E extends Entity<E>> extends StatelessWidget {
-//   final E entity;
-//
-//   EntityWidget({Key? key, required this.entity}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text(entity.concept.label ?? 'Entity')),
-//       body: SingleChildScrollView(
-//         child: Column(
-//           children: [
-//             ...buildAttributeWidgets(entity, context),
-//             const Divider(),
-//             ...buildRelationshipWidgets(entity),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-//
-//   List<Widget> buildAttributeWidgets(E entity, BuildContext context) {
-//     return entity.concept.attributes.map<Widget>((attribute) {
-//       Widget field;
-//       switch (attribute.type) {
-//         case 'DateTime':
-//           field = TextFormField(
-//             initialValue:
-//                 entity.getAttribute<DateTime>(attribute.code)?.toString(),
-//             decoration: InputDecoration(labelText: attribute.label),
-//             onTap: () async {
-//               DateTime? picked = await showDatePicker(
-//                 context: context,
-//                 initialDate: entity.getAttribute<DateTime>(attribute.code) ??
-//                     DateTime.now(),
-//                 firstDate: DateTime(2000),
-//                 lastDate: DateTime(2100),
-//               );
-//               if (picked != null) entity.setAttribute(attribute.code, picked);
-//             },
-//           );
-//           break;
-//         case 'bool':
-//           field = Switch(
-//             value: entity.getAttribute<bool>(attribute.code) ?? false,
-//             onChanged: (bool value) {
-//               entity.setAttribute(attribute.code, value);
-//             },
-//           );
-//           break;
-//         case 'int':
-//         case 'double':
-//         case 'String':
-//         default:
-//           field = TextFormField(
-//             initialValue: entity.getAttribute<String>(attribute.code),
-//             decoration: InputDecoration(labelText: attribute.label),
-//             onChanged: (value) => entity.setAttribute(attribute.code, value),
-//           );
-//           break;
-//       }
-//       return ListTile(
-//           title: Text(attribute.label ?? attribute.code), subtitle: field);
-//     }).toList();
-//   }
-//
-//   List<Widget> buildRelationshipWidgets(E entity) {
-//     List<Widget> widgets = [];
-//     for (Parent parent in entity.concept.parents as List<Parent>) {
-//       widgets.add(ExpansionTile(
-//         title: Text('Parent: ${parent.label ?? parent.code}'),
-//         children: <Widget>[
-//           Text(entity.getParent(parent.code ?? '')?.toString() ?? 'None'),
-//         ],
-//       ));
-//     }
-//     for (Child child in entity.concept.children as List<Child>) {
-//       widgets.add(ExpansionTile(
-//         title: Text('Child: ${child.label ?? child.code}'),
-//         children: <Widget>[
-//           Text(entity.getChild(child.code)?.toString() ?? 'None'),
-//         ],
-//       ));
-//     }
-//     return widgets;
-//   }
-// }
+part of ednet_cms;
+
+// Widget for String attribute
+class StringAttributeWidget extends StatelessWidget {
+  final String label;
+  final String value;
+  final Function(String) onChanged;
+
+  StringAttributeWidget(
+      {required this.label, required this.value, required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(labelText: label),
+      controller: TextEditingController(text: value),
+      onChanged: onChanged,
+    );
+  }
+}
+
+// Widget for int attribute
+class IntAttributeWidget extends StatelessWidget {
+  final String label;
+  final int value;
+  final Function(int) onChanged;
+
+  IntAttributeWidget(
+      {required this.label, required this.value, required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(labelText: label),
+      controller: TextEditingController(text: value.toString()),
+      keyboardType: TextInputType.number,
+      onChanged: (text) => onChanged(int.tryParse(text) ?? 0),
+    );
+  }
+}
+
+// Widget for double attribute
+class DoubleAttributeWidget extends StatelessWidget {
+  final String label;
+  final double value;
+  final Function(double) onChanged;
+
+  DoubleAttributeWidget(
+      {required this.label, required this.value, required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(labelText: label),
+      controller: TextEditingController(text: value.toString()),
+      keyboardType: TextInputType.number,
+      onChanged: (text) => onChanged(double.tryParse(text) ?? 0.0),
+    );
+  }
+}
+
+// Widget for bool attribute
+class BoolAttributeWidget extends StatelessWidget {
+  final String label;
+  final bool value;
+  final Function(bool) onChanged;
+
+  BoolAttributeWidget(
+      {required this.label, required this.value, required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(label),
+        Switch(
+          value: value,
+          onChanged: onChanged,
+        ),
+      ],
+    );
+  }
+}
+
+// Widget for DateTime attribute
+class DateTimeAttributeWidget extends StatelessWidget {
+  final String label;
+  final DateTime value;
+  final Function(DateTime) onChanged;
+
+  DateTimeAttributeWidget(
+      {required this.label, required this.value, required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(label),
+        TextButton(
+          onPressed: () async {
+            DateTime? picked = await showDatePicker(
+              context: context,
+              initialDate: value,
+              firstDate: DateTime(2000),
+              lastDate: DateTime(2100),
+            );
+            if (picked != null && picked != value) onChanged(picked);
+          },
+          child: Text(value.toLocal().toString().split(' ')[0]),
+        ),
+      ],
+    );
+  }
+}
+
+class EntityWidget extends StatelessWidget {
+  final Entity entity;
+
+  EntityWidget({required this.entity});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: entity.concept.attributes.map((attribute) {
+          var value = entity.getAttribute(attribute.code);
+          switch (attribute.type?.code) {
+            case 'String':
+              return StringAttributeWidget(
+                label: attribute.code,
+                value: value as String,
+                onChanged: (newValue) {
+                  entity.setAttribute(attribute.code, newValue);
+                },
+              );
+            case 'int':
+              return IntAttributeWidget(
+                label: attribute.code,
+                value: value as int,
+                onChanged: (newValue) {
+                  entity.setAttribute(attribute.code, newValue);
+                },
+              );
+            case 'double':
+              return DoubleAttributeWidget(
+                label: attribute.code,
+                value: value as double,
+                onChanged: (newValue) {
+                  entity.setAttribute(attribute.code, newValue);
+                },
+              );
+            case 'bool':
+              return BoolAttributeWidget(
+                label: attribute.code,
+                value: value as bool,
+                onChanged: (newValue) {
+                  entity.setAttribute(attribute.code, newValue);
+                },
+              );
+            case 'DateTime':
+              return DateTimeAttributeWidget(
+                label: attribute.code,
+                value: value as DateTime,
+                onChanged: (newValue) {
+                  entity.setAttribute(attribute.code, newValue);
+                },
+              );
+            default:
+              return Container();
+          }
+        }).toList(),
+      ),
+    );
+  }
+}
