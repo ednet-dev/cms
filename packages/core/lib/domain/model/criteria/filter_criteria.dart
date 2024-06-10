5fda28239f6c76c4854cda18dbeb911f70923e54
slavisam <slavisam@gmail.com>
2024-06-11 00:56:03 +0200

    impl(core): Filter Criteria.

---
+++ packages/core/lib/domain/model/criteria/filter_criteria.dart
@@ -0,0 +1,10 @@
+part of ednet_core;
+
+class FilterCriteria {
+  final String attribute;
+  final String operator;
+  final dynamic value;
+
+  FilterCriteria(
+      {required this.attribute, required this.operator, required this.value});
+}
