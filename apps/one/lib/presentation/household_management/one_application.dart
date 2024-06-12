c0d32110429c4b90739135a204e403859680591e
slavisam <slavisam@gmail.com>
2024-06-12 23:04:20 +0200

    domain(one): Extend suffixes in OneApplication.

--- apps/one/lib/presentation/household_management/one_application.dart
+++ apps/one/lib/presentation/household_management/one_application.dart
@@ -1,19 +1,19 @@
 import 'package:ednet_core/ednet_core.dart';
 import 'package:ednet_one/generated/hausehold/finance/lib/household_finance.dart'
-    as hf;
+    as hdfe;
 import 'package:ednet_one/generated/hausehold/member/lib/household_member.dart'
-    as hm;
+    as hdmr;
 import 'package:ednet_one/generated/project/brainstorming/lib/project_brainstorming.dart'
-    as pb;
-import 'package:ednet_one/generated/project/core/lib/project_core.dart' as pc;
-import 'package:ednet_one/generated/project/gtd/lib/project_gtd.dart' as pg;
+    as ptbg;
+import 'package:ednet_one/generated/project/core/lib/project_core.dart' as ptce;
+import 'package:ednet_one/generated/project/gtd/lib/project_gtd.dart' as ptgd;
 import 'package:ednet_one/generated/project/kanban/lib/project_kanban.dart'
-    as pk;
+    as ptkn;
 import 'package:ednet_one/generated/project/planning/lib/project_planning.dart'
-    as pp;
+    as ptpg;
 import 'package:ednet_one/generated/project/scheduling/lib/project_scheduling.dart'
-    as ps;
-import 'package:ednet_one/generated/project/user/lib/project_user.dart' as pu;
+    as ptsg;
+import 'package:ednet_one/generated/project/user/lib/project_user.dart' as ptur;
 
 class OneApplication {
   final Domains _domains = Domains();
@@ -24,74 +24,74 @@ class OneApplication {
 
   void _initializeDomains() {
     // household finance
-    final householdFinanceRepo = hf.HouseholdFinanceRepo();
-    hf.HouseholdDomain householdFinanceDomain =
-        householdFinanceRepo.getDomainModels("Household") as hf.HouseholdDomain;
-    hf.FinanceModel financeModel =
-        householdFinanceDomain.getModelEntries("Finance") as hf.FinanceModel;
+    final householdFinanceRepo = hdfe.HouseholdFinanceRepo();
+    hdfe.HouseholdDomain householdFinanceDomain = householdFinanceRepo
+        .getDomainModels("Household") as hdfe.HouseholdDomain;
+    hdfe.FinanceModel financeModel =
+        householdFinanceDomain.getModelEntries("Finance") as hdfe.FinanceModel;
     financeModel.init();
 
     // household member
-    final householdMemberRepo = hm.HouseholdMemberRepo();
-    hm.HouseholdDomain householdMemberDomain =
-        householdMemberRepo.getDomainModels("Household") as hm.HouseholdDomain;
-    hm.MemberModel memberModel =
-        householdMemberDomain.getModelEntries("Member") as hm.MemberModel;
+    final householdMemberRepo = hdmr.HouseholdMemberRepo();
+    hdmr.HouseholdDomain householdMemberDomain = householdMemberRepo
+        .getDomainModels("Household") as hdmr.HouseholdDomain;
+    hdmr.MemberModel memberModel =
+        householdMemberDomain.getModelEntries("Member") as hdmr.MemberModel;
     memberModel.init();
 
     // project brainstorming
-    final projectBrainstormingRepo = pb.ProjectBrainstormingRepo();
-    pb.ProjectDomain projectBrainstormingDomain =
-        projectBrainstormingRepo.getDomainModels("Project") as pb.ProjectDomain;
-    pb.BrainstormingModel brainstormingModel = projectBrainstormingDomain
-        .getModelEntries("Brainstorming") as pb.BrainstormingModel;
+    final projectBrainstormingRepo = ptbg.ProjectBrainstormingRepo();
+    ptbg.ProjectDomain projectBrainstormingDomain = projectBrainstormingRepo
+        .getDomainModels("Project") as ptbg.ProjectDomain;
+    ptbg.BrainstormingModel brainstormingModel = projectBrainstormingDomain
+        .getModelEntries("Brainstorming") as ptbg.BrainstormingModel;
     brainstormingModel.init();
 
     // project core
-    final projectCoreRepo = pc.ProjectCoreRepo();
-    pc.ProjectDomain projectCoreDomain =
-        projectCoreRepo.getDomainModels("Project") as pc.ProjectDomain;
-    pc.CoreModel coreModel =
-        projectCoreDomain.getModelEntries("Core") as pc.CoreModel;
+    final projectCoreRepo = ptce.ProjectCoreRepo();
+    ptce.ProjectDomain projectCoreDomain =
+        projectCoreRepo.getDomainModels("Project") as ptce.ProjectDomain;
+    ptce.CoreModel coreModel =
+        projectCoreDomain.getModelEntries("Core") as ptce.CoreModel;
     coreModel.init();
 
     // project gtd
-    final projectGtdRepo = pg.ProjectGtdRepo();
-    pg.ProjectDomain projectGtdDomain =
-        projectGtdRepo.getDomainModels("Project") as pg.ProjectDomain;
-    pg.GtdModel gtdModel =
-        projectGtdDomain.getModelEntries("Gtd") as pg.GtdModel;
+    final projectGtdRepo = ptgd.ProjectGtdRepo();
+    ptgd.ProjectDomain projectGtdDomain =
+        projectGtdRepo.getDomainModels("Project") as ptgd.ProjectDomain;
+    ptgd.GtdModel gtdModel =
+        projectGtdDomain.getModelEntries("Gtd") as ptgd.GtdModel;
 
     // project kanban
-    final projectKanbanRepo = pk.ProjectKanbanRepo();
-    pk.ProjectDomain projectKanbanDomain =
-        projectKanbanRepo.getDomainModels("Project") as pk.ProjectDomain;
-    pk.KanbanModel kanbanModel =
-        projectKanbanDomain.getModelEntries("Kanban") as pk.KanbanModel;
+    final projectKanbanRepo = ptkn.ProjectKanbanRepo();
+    ptkn.ProjectDomain projectKanbanDomain =
+        projectKanbanRepo.getDomainModels("Project") as ptkn.ProjectDomain;
+    ptkn.KanbanModel kanbanModel =
+        projectKanbanDomain.getModelEntries("Kanban") as ptkn.KanbanModel;
     kanbanModel.init();
 
     // project planning
-    final projectPlanningRepo = pp.ProjectPlanningRepo();
-    pp.ProjectDomain projectPlanningDomain =
-        projectPlanningRepo.getDomainModels("Project") as pp.ProjectDomain;
-    pp.PlanningModel planningModel =
-        projectPlanningDomain.getModelEntries("Planning") as pp.PlanningModel;
+    final projectPlanningRepo = ptpg.ProjectPlanningRepo();
+    ptpg.ProjectDomain projectPlanningDomain =
+        projectPlanningRepo.getDomainModels("Project") as ptpg.ProjectDomain;
+    ptpg.PlanningModel planningModel =
+        projectPlanningDomain.getModelEntries("Planning") as ptpg.PlanningModel;
     planningModel.init();
 
     // project scheduling
-    final projectSchedulingRepo = ps.ProjectSchedulingRepo();
-    ps.ProjectDomain projectSchedulingDomain =
-        projectSchedulingRepo.getDomainModels("Project") as ps.ProjectDomain;
-    ps.SchedulingModel schedulingModel = projectSchedulingDomain
-        .getModelEntries("Scheduling") as ps.SchedulingModel;
+    final projectSchedulingRepo = ptsg.ProjectSchedulingRepo();
+    ptsg.ProjectDomain projectSchedulingDomain =
+        projectSchedulingRepo.getDomainModels("Project") as ptsg.ProjectDomain;
+    ptsg.SchedulingModel schedulingModel = projectSchedulingDomain
+        .getModelEntries("Scheduling") as ptsg.SchedulingModel;
     schedulingModel.init();
 
     // project user
-    final projectUserRepo = pu.ProjectUserRepo();
-    pu.ProjectDomain projectUserDomain =
-        projectUserRepo.getDomainModels("Project") as pu.ProjectDomain;
-    pu.UserModel userModel =
-        projectUserDomain.getModelEntries("User") as pu.UserModel;
+    final projectUserRepo = ptur.ProjectUserRepo();
+    ptur.ProjectDomain projectUserDomain =
+        projectUserRepo.getDomainModels("Project") as ptur.ProjectDomain;
+    ptur.UserModel userModel =
+        projectUserDomain.getModelEntries("User") as ptur.UserModel;
     userModel.init();
 
     _domains
