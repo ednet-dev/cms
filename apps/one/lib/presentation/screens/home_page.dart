86d3e82c993096b7032ce23bbd960bbf30662b3f
slavisam <slavisam@gmail.com>
2024-06-15 23:12:00 +0200

    Alg

--- apps/one/lib/presentation/screens/home_page.dart
+++ apps/one/lib/presentation/screens/home_page.dart
@@ -95,101 +95,120 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.view_quilt),
-            onPressed: () {
-              setState(() {
-                showMetaCanvas = !showMetaCanvas;
-              });
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.swap_horiz),
-            onPressed: () {
-              context.read<LayoutBloc>().add(ToggleLayoutEvent());
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.brightness_6),
-            onPressed: () {
-              BlocProvider.of<ThemeBloc>(context).toggleTheme();
-            },
-          ),
-        ],
-      ),
-      body: BlocProvider(
-        create: (context) => LayoutBloc(),
-        child: BlocBuilder<LayoutBloc, LayoutState>(
-          builder: (context, state) {
-            if (showMetaCanvas) {
-              return Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      LayoutAlgorithmIcon(
-                        icon: Icons.auto_fix_high,
-                        name: 'Force Directed',
-                        onTap: () => _changeLayoutAlgorithm(
-                            ForceDirectedLayoutAlgorithm() as LayoutAlgorithm),
-                      ),
-                      LayoutAlgorithmIcon(
-                        icon: Icons.grid_on,
-                        name: 'Grid',
-                        onTap: () => _changeLayoutAlgorithm(
-                            GridLayoutAlgorithm() as LayoutAlgorithm),
-                      ),
-                      LayoutAlgorithmIcon(
-                        icon: Icons.circle,
-                        name: 'Circular',
-                        onTap: () => _changeLayoutAlgorithm(
-                            CircularLayoutAlgorithm() as LayoutAlgorithm),
-                      ),
-                      LayoutAlgorithmIcon(
-                        icon: Icons.format_indent_increase,
-                        name: 'Master Detail',
-                        onTap: () => _changeLayoutAlgorithm(
-                            MasterDetailLayoutAlgorithm() as LayoutAlgorithm),
-                      ),
-                    ],
-                  ),
-                  Expanded(
-                    child: MetaDomainCanvas(
-                      domains: app.domains,
-                      layoutAlgorithm: _selectedAlgorithm,
-                      decorators: [],
-                    ),
-                  ),
-                ],
-              );
-            } else {
-              return Row(
-                children: [
-                  LeftSidebarWidget(
-                    domains: app.domains,
-                    onDomainSelected: _handleDomainSelected,
-                  ),
-                  if (selectedDomain != null)
-                    RightSidebarWidget(
-                      models: selectedDomain!.models,
-                      onModelSelected: _handleModelSelected,
-                    ),
-                  if (selectedEntries != null)
-                    EntriesSidebarWidget(
-                      entries: selectedEntries!,
-                    ),
-                ],
-              );
-            }
-          },
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.view_quilt),
+              onPressed: () {
+                setState(() {
+                  showMetaCanvas = !showMetaCanvas;
+                });
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.swap_horiz),
+              onPressed: () {
+                context.read<LayoutBloc>().add(ToggleLayoutEvent());
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.brightness_6),
+              onPressed: () {
+                BlocProvider.of<ThemeBloc>(context).toggleTheme();
+              },
+            ),
+          ],
         ),
-      ),
-    );
+        body: BlocProvider(
+          create: (context) => LayoutBloc(),
+          child: BlocBuilder<LayoutBloc, LayoutState>(
+            builder: (context, state) {
+              if (showMetaCanvas) {
+                return Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        LayoutAlgorithmIcon(
+                          icon: Icons.auto_fix_high,
+                          name: 'Force Directed',
+                          onTap: () => _changeLayoutAlgorithm(
+                              ForceDirectedLayoutAlgorithm()
+                                  as LayoutAlgorithm),
+                        ),
+                        LayoutAlgorithmIcon(
+                          icon: Icons.grid_on,
+                          name: 'Grid',
+                          onTap: () => _changeLayoutAlgorithm(
+                              GridLayoutAlgorithm() as LayoutAlgorithm),
+                        ),
+                        LayoutAlgorithmIcon(
+                          icon: Icons.circle,
+                          name: 'Circular',
+                          onTap: () => _changeLayoutAlgorithm(
+                              CircularLayoutAlgorithm() as LayoutAlgorithm),
+                        ),
+                        LayoutAlgorithmIcon(
+                          icon: Icons.format_indent_increase,
+                          name: 'Master Detail',
+                          onTap: () => _changeLayoutAlgorithm(
+                              MasterDetailLayoutAlgorithm() as LayoutAlgorithm),
+                        ),
+                      ],
+                    ),
+                    Expanded(
+                      child: MetaDomainCanvas(
+                        domains: app.domains,
+                        layoutAlgorithm: _selectedAlgorithm,
+                        decorators: [],
+                      ),
+                    ),
+                  ],
+                );
+              } else {
+                return Row(
+                  children: [
+                    LeftSidebarWidget(
+                      domains: app.domains,
+                      onDomainSelected: _handleDomainSelected,
+                    ),
+                    if (selectedDomain != null)
+                      RightSidebarWidget(
+                        models: selectedDomain!.models,
+                        onModelSelected: _handleModelSelected,
+                      ),
+                    if (selectedEntries != null)
+                      EntriesSidebarWidget(
+                        entries: selectedEntries!,
+                      ),
+                  ],
+                );
+              }
+            },
+          ),
+        ),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                // Handle zoom in
+              },
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              child: Icon(Icons.add, color: Colors.white),
+            ),
+            SizedBox(width: 16.0),
+            FloatingActionButton(
+              onPressed: () {
+                // Handle zoom out
+              },
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              child: Icon(Icons.remove, color: Colors.white),
+            ),
+          ],
+        ));
   }
 }
-
-
-
