34cb20578337a0f91f01c29b3f554df66ccda80f
dev@ednet.dev <dev@ednet.dev>
2025-04-03 21:04:44 +0200

    refactor(direct_democracy): remove unused proposal and comment files
    - Deleted obsolete files related to the direct democracy proposal and comment functionalities, including `README.md`, `comment.dart`, `proposal.dart`, `vote.dart`, and `proposal_service.dart`.
    - This cleanup helps streamline the codebase by removing unnecessary components that are no longer in use.

--- packages/core/test/policy/model_policy_test.dart
+++ packages/core/test/policy/model_policy_test.dart
@@ -15,27 +15,32 @@ void main() {
     testDomain = TestDomain(domain);
     testEntity = TestEntity(testDomain.testConcept);
 
-    concept.attributes
-        .add(Attribute(concept, 'age')..type = AttributeType(domain, 'int'));
     concept.attributes.add(
-        Attribute(concept, 'name')..type = AttributeType(domain, 'String'));
+      Attribute(concept, 'age')..type = AttributeType(domain, 'int'),
+    );
+    concept.attributes.add(
+      Attribute(concept, 'name')..type = AttributeType(domain, 'String'),
+    );
     model.concepts.add(concept);
   });
 
   test('Entity Attribute Change Policy Evaluation', () {
     var agePolicy = AttributePolicy(
-        name: 'Age Policy',
-        description: 'Age must be between 18 and 100',
-        attributeName: 'age',
-        validator: AttributeValidators.isBetween(18, 100));
+      name: 'Age Policy',
+      description: 'Age must be between 18 and 100',
+      attributeName: 'age',
+      validator: AttributeValidators.isBetween(18, 100),
+    );
 
     model.registerPolicy('agePolicy', agePolicy);
 
     expect(() => testEntity.setAttribute('age', 1), returnsNormally);
     expect(testEntity.getAttribute('age'), equals(1));
 
-    expect(() => testEntity.setAttribute('age', 15),
-        throwsA(isA<PolicyViolationException>()));
+    expect(
+      () => testEntity.setAttribute('age', 15),
+      throwsA(isA<PolicyViolationException>()),
+    );
     expect(testEntity.getAttribute('age'), equals(25));
   });
 
@@ -45,19 +50,22 @@ void main() {
     var parent = TestEntity(parentConcept);
 
     var parentPolicy = RelationshipPolicy(
-        name: 'Parent Policy',
-        description: 'Entity must have a parent',
-        relationshipName: 'parent',
-        relationshipType: RelationshipType.parent,
-        validator: RelationshipValidators.isNotNull);
+      name: 'Parent Policy',
+      description: 'Entity must have a parent',
+      relationshipName: 'parent',
+      relationshipType: RelationshipType.parent,
+      validator: RelationshipValidators.isNotNull,
+    );
 
     model.registerPolicy('parentPolicy', parentPolicy);
 
     expect(() => testEntity.setParent('parent', parent), returnsNormally);
     expect(testEntity.getParent('parent'), equals(parent));
 
-    expect(() => testEntity.setParent('parent', null),
-        throwsA(isA<PolicyViolationException>()));
+    expect(
+      () => testEntity.setParent('parent', null),
+      throwsA(isA<PolicyViolationException>()),
+    );
     expect(testEntity.getParent('parent'), equals(parent));
   });
 
