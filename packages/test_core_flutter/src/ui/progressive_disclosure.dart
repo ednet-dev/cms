34530df542994482024d46031598e2c4921e023a
dev@ednet.dev <dev@ednet.dev>
2025-04-16 23:53:02 +0200

    Cleanup

--- packages/test_core_flutter/src/ui/progressive_disclosure.dart
+++
@@ -1,8 +0,0 @@
-part of 'package:ednet_core_flutter/ednet_core_flutter.dart';
-
-/// Interface for UI components that support progressive disclosure levels,
-/// allowing the UI to adapt based on user expertise
-mixin ProgressiveDisclosure {
-  /// The current disclosure level for progressive UI
-  DisclosureLevel get disclosureLevel;
-}
